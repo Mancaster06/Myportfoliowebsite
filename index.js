d7e074351cb91d40bd27ad5f182802c778b78992
Mancaster06 <rajabuomary061@gmail.com>
2024-03-27 04:19:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let myBook = {
+
+    title: "JavaScript Introduction",
+    
+    author: "Nathan Sebhastian",
+    
+    };
+    myBook.year =2024;
+    myBook["Publishers"] = "CodewithRajabu";
+    console.log("title" in myBook)
+    
+    
+   
